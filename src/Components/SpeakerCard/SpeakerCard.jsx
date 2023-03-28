a499d084ae78c9136374ea72f90f579ea2883971
JOSU9435 <kamalnetra702@gmail.com>
2023-03-28 14:29:18 +0530

    UPD: resize speaker card

--- src/Components/SpeakerCard/SpeakerCard.jsx
+++ src/Components/SpeakerCard/SpeakerCard.jsx
@@ -50,7 +50,9 @@ const SpeakerCard = ({ img, name, desg, socials }) => {
         )`,
       }}
     >
-      <img className={styles["card-img"]} src={img} alt={name} />
+      <div className={styles["card-img-wrapper"]}>
+        <img className={styles["card-img"]} src={img} alt={name} />
+      </div>
       <div className={styles["card-details"]}>
         <span className={styles["card-name"]}>{name}</span>
         <span className={styles["card-desg"]}>{desg}</span>
