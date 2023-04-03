4f07db965e0d056fb055b95ec54713e7872691e5
username-naz <duttaaniruddha2001@gmail.com>
2023-04-03 20:09:14 +0530

    add: required links in registration page

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,6 +1,6 @@
 import { useLayoutEffect, useEffect } from "react";
 import style from "./Registration.module.scss";
-import { Sponsor } from "../../Components";
+import { Sponsor, Button } from "../../Components";
 
 const Registration = ({ onLoad }) => {
   useLayoutEffect(() => {
@@ -23,12 +23,41 @@ const Registration = ({ onLoad }) => {
       <div className={style["nits-reg"]}>
         <div className={style.heading}>NIT SILCHAR PHASE 1 REGISTRATION</div>
         <div className={style.about}>
-          GDSC Explore Phase 1 for all NIT Silchar Students is now live through Devfolio!
-          Submit your Ideation according to the proper format by 6th April. Selected teams
-          from the ideation round will be eligible for the technical round, which will
-          start on 7th April. All participants are asked to follow the rules and
-          guidelines mentioned here.
+          GDSC Explore Phase 1 for all NIT Silchar Students is now live! Submit your
+          Ideation according to the{" "}
+          <a
+            href="https://docs.google.com/document/d/1qwLDoE2fbb_wES1wn3ZUO1kJaVcKfpK6XVJhUSMG8tQ/edit?usp=sharing"
+            target="_blank"
+          >
+            proper format
+          </a>{" "}
+          by 6th April. Selected teams from the ideation round will be eligible for the
+          technical round, which will start on 7th April. All participants are asked to
+          follow the rules and guidelines mentioned{" "}
+          <a
+            href="https://docs.google.com/document/d/13PuDOEJ9kOeYFVgQge4vqfaZmhosXEEb3DccmIgkJu4/edit"
+            target="_blank"
+          >
+            here
+          </a>
+          .
+          <br /> <br />
+          Want to know more about Explore?{" "}
+          <a href="https://gdsc.community.dev/j/nvbwt2fs98fny/" target="_blank">
+            RSVP for our Info Session
+          </a>{" "}
+          on 6th April.
         </div>
+        <a
+          href="https://forms.gle/ptMaxNh3z9d12Ak49"
+          target="_blank"
+          rel="noreferrer"
+          className={style["devfolio-btn"]}
+        >
+          <Button primary="primary" size="large">
+            Register
+          </Button>
+        </a>
         <div className={style["devfolio-btn"]}>
           <div
             className="apply-button"
